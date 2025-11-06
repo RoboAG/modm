415bef84c8ab1038083805d1c0105d533b343b11
Janek Neugebauer <238980996+janekngbr@users.noreply.github.com>
2025-11-06 21:58:49 +0100

    Updated example to use FixedFrequencyPwm class

--- examples/avr/pwm/timer_0/main.cpp
+++ examples/avr/pwm/timer_0/main.cpp
@@ -1,28 +1,35 @@
-#include <modm/platform/timer/timer_0.hpp>
-#include <modm/platform/gpio/pins.hpp>
 #include <modm/architecture/interface/delay.hpp>
+#include <modm/platform/gpio/pins.hpp>
+#include <modm/platform/timer/timer_0.hpp>
 
 // repeatedly fades an LED connected to OC0A (pin D6) on and off
-int main() {
+int
+main()
+{
 	using Timer = modm::platform::Timer0;
+	using Pwm =
+		Timer::FixedFrequencyPwm<Timer::PwmMode::FastPwm, Timer::ClockSource::ClkIoDiv64, 0xff>;
 
 	// results in a PWM frequency of 976.5 Hz at F_CPU = 16 MHz
-	Timer::initialize(Timer::WaveformGenerationMode::FastPwm8Bit, Timer::ClockSource::ClkIoDiv64);
+	Pwm::initialize();
 
 	modm::platform::GpioD6::setOutput();
-	Timer::OutputChannelA::connect<modm::platform::GpioD6::Oca>(Timer::OutputMode::PwmNormal);
+	Pwm::ChannelA::connect<modm::platform::GpioD6::Oca>();
 
-	while (true) {
-		for (uint8_t dutyCycle = 0; dutyCycle < 255; ++dutyCycle) {
-			Timer::OutputChannelA::setOutputCompareRegister(dutyCycle);
+	while (true)
+	{
+		for (uint8_t dutyCycle = 0; dutyCycle < 255; ++dutyCycle)
+		{
+			Pwm::ChannelA::setDutyCycle(dutyCycle);
 
 			// need to use delay instead of anything based on modm:platform:clock
 			// because that would use timer 0 which is required for the PWM generation instead
 			modm::delay_ms(4);
 		}
 
-		for (uint8_t dutyCycle = 255; dutyCycle > 0; --dutyCycle) {
-			Timer::OutputChannelA::setOutputCompareRegister(dutyCycle);
+		for (uint8_t dutyCycle = 255; dutyCycle > 0; --dutyCycle)
+		{
+			Pwm::ChannelA::setDutyCycle(dutyCycle);
 
 			modm::delay_ms(4);
 		}
